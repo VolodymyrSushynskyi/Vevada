f8388daaae0c5aff8c9f8edf8b0bccb50ec2fe53
AruitEden <annaleks1205@gmail.com>
2026-05-18 15:58:42 +0300

    feat: add login form (#46)

--- src/frontend/src/app/features/auth/components/auth-wrapper/auth-wrapper.ts
+++ src/frontend/src/app/features/auth/components/auth-wrapper/auth-wrapper.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubButton } from '../../../../shared/components/sub-button/sub-button';
 import { RegisterForm } from '../register-form/register-form';
+import { LoginForm } from '../login-form/login-form';
 
 @Component({
   selector: 'app-auth-wrapper',
   standalone: true,
-  imports: [CommonModule, SubButton, RegisterForm],
+  imports: [CommonModule, SubButton, RegisterForm, LoginForm],
   templateUrl: './auth-wrapper.html',
   styleUrl: './auth-wrapper.css',
 })
