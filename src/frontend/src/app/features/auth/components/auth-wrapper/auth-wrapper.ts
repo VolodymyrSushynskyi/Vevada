d5f78900f94ab0bd5d44e82be948931424a435e8
AruitEden <annaleks1205@gmail.com>
2026-05-18 15:16:26 +0300

    feat: add multi-step register form(#46)

--- src/frontend/src/app/features/auth/components/auth-wrapper/auth-wrapper.ts
+++ src/frontend/src/app/features/auth/components/auth-wrapper/auth-wrapper.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubButton } from '../../../../shared/components/sub-button/sub-button';
+import { RegisterForm } from '../register-form/register-form';
 
 @Component({
   selector: 'app-auth-wrapper',
   standalone: true,
-  imports: [CommonModule, SubButton],
+  imports: [CommonModule, SubButton, RegisterForm],
   templateUrl: './auth-wrapper.html',
   styleUrl: './auth-wrapper.css',
 })
