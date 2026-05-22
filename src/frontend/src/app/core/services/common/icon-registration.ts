123c3b75284036f03db7223f6b05e59ec200d748
AruitEden <annaleks1205@gmail.com>
2026-05-22 22:48:47 +0300

    fix: configure static output and prevent icon load on server

--- src/frontend/src/app/core/services/common/icon-registration.ts
+++ src/frontend/src/app/core/services/common/icon-registration.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -9,22 +10,25 @@ export class IconRegistration {
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
+    @Inject(PLATFORM_ID) private platformId: Object,
   ) {}
 
   public registerIcons(): void {
-    const icons = [
-      { name: 'icon-close', path: 'icons/x.svg' },
-      { name: 'icon-arrow-right', path: 'icons/arrow-right.svg' },
-      { name: 'icon-arrow-left', path: 'icons/arrow-left.svg' },
-      { name: 'icon-white-logo', path: 'icons/white-logo.svg' },
-      { name: 'icon-green-logo', path: 'icons/green-logo.svg' },
-    ];
+    if (isPlatformBrowser(this.platformId)) {
+      const icons = [
+        { name: 'icon-close', path: 'icons/x.svg' },
+        { name: 'icon-arrow-right', path: 'icons/arrow-right.svg' },
+        { name: 'icon-arrow-left', path: 'icons/arrow-left.svg' },
+        { name: 'icon-white-logo', path: 'icons/white-logo.svg' },
+        { name: 'icon-green-logo', path: 'icons/green-logo.svg' },
+      ];
 
-    icons.forEach((icon) => {
-      this.matIconRegistry.addSvgIcon(
-        icon.name,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(icon.path),
-      );
-    });
+      icons.forEach((icon) => {
+        this.matIconRegistry.addSvgIcon(
+          icon.name,
+          this.domSanitizer.bypassSecurityTrustResourceUrl(icon.path),
+        );
+      });
+    }
   }
 }
