6d209d8177d05cdd4ecd070d8dceca3370dd3d4f
Hanna Oleksiienko <111302550+AruitEden@users.noreply.github.com>
2026-05-24 11:05:34 +0300

    Revert "feature-63: implement header component (#71)" (#72)
    This reverts commit f6a77507f9928ec38ceb81f0c983a5e56db9f8fc.

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './features/client/pages/home/home';
 import { ClientAuth } from './features/auth/pages/client-auth/client-auth';
 import { AdminAuth } from './features/auth/pages/admin-auth/admin-auth';
-import { ClientLayout } from './features/client/components/client-layout/client-layout';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ClientLayout,
-    children: [{ path: '', component: Home }],
+    component: Home,
   },
   { path: 'login', component: ClientAuth },
   {
