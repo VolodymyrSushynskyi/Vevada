cae9b09b8ca4f7a065759c8f65d612cc85cde2ad
SushynskyiVolodymyr <sushinskyvladimir@gmail.com>
2026-05-27 01:47:21 +0300

    Implement data layer

---
+++ src/backend/Vevada/Vevada.Data/Configurations/AdminDetailsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vevada.Data.Entities;
+
+namespace Vevada.Data.Configurations;
+
+public class AdminDetailsConfiguration : IEntityTypeConfiguration<AdminDetails>
+{
+    private const int FirstNameMaxLength = 50;
+    private const int LastNameMaxLength = 50;
+
+    public void Configure(EntityTypeBuilder<AdminDetails> builder)
+    {
+        builder.HasKey(ad => ad.UserId);
+
+        builder.Property(ad => ad.FirstName)
+            .IsRequired()
+            .HasMaxLength(FirstNameMaxLength);
+
+        builder.Property(ad => ad.LastName)
+            .IsRequired()
+            .HasMaxLength(LastNameMaxLength);
+
+        builder.HasOne(ad => ad.User)
+            .WithOne()
+            .HasForeignKey<AdminDetails>(ad => ad.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
