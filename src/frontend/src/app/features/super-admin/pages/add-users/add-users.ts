9801942e3ab0d436842ec3f22cf0ed5c90e3c381
AruitEden <annaleks1205@gmail.com>
2026-05-27 15:28:07 +0300

    feat-93: add role chip

--- src/frontend/src/app/features/super-admin/pages/add-users/add-users.ts
+++ src/frontend/src/app/features/super-admin/pages/add-users/add-users.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RoleChip } from '../../components/role-chip/role-chip';
 
 @Component({
   selector: 'app-add-users',
-  imports: [],
+  imports: [RoleChip],
   templateUrl: './add-users.html',
   styleUrl: './add-users.css',
 })
