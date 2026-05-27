805afd7fcce933916ac709f3eb738cb0bdccd3c8
AruitEden <annaleks1205@gmail.com>
2026-05-27 18:35:37 +0300

    feat-93: created add users dialog

--- src/frontend/src/app/features/super-admin/pages/add-users/add-users.ts
+++ src/frontend/src/app/features/super-admin/pages/add-users/add-users.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-import { RoleChip } from '../../components/role-chip/role-chip';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { AddUserDialog } from '../../components/add-user-dialog/add-user-dialog';
+import { MainButton } from '../../../../shared/components/main-button/main-button';
 
 @Component({
   selector: 'app-add-users',
-  imports: [RoleChip],
+  imports: [CommonModule, MainButton],
   templateUrl: './add-users.html',
   styleUrl: './add-users.css',
 })
-export class AddUsers {}
+export class AddUsers {
+  private dialog = inject(MatDialog);
+
+  openAddUserDialog() {
+    const dialogRef = this.dialog.open(AddUserDialog, {
+      maxWidth: '100vw',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log('Отримані дані з форми:', result);
+      }
+    });
+  }
+}
