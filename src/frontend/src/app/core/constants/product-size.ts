32ebc57881901814d0c9a38673435c25f726aaa6
AruitEden <annaleks1205@gmail.com>
2026-05-30 15:49:09 +0300

    add DTO models

---
+++ src/frontend/src/app/core/constants/product-size.ts
@@ -0,0 +1,13 @@
+export enum ProductSize {
+  Size98 = 98,
+  Size110 = 110,
+  Size116 = 116,
+  Size122 = 122,
+  Size128 = 128,
+  Size134 = 134,
+  Size140 = 140,
+  Size146 = 146,
+  Size152 = 152,
+  Size160 = 160,
+  Size168 = 168,
+}
