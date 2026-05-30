2dada17ae06bf2c2aa2d425ce16402f11d5a0760
AruitEden <annaleks1205@gmail.com>
2026-05-30 23:58:09 +0300

    updated product card

--- src/frontend/src/app/features/client/components/catalog-grid/catalog-grid.ts
+++ src/frontend/src/app/features/client/components/catalog-grid/catalog-grid.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ProductCard } from '../product-card/product-card';
+import { CatalogProductDto } from '../../../../core/models/catalog-product.models';
 
 @Component({
   selector: 'app-catalog-grid',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ProductCard, MatProgressSpinnerModule],
   templateUrl: './catalog-grid.html',
   styleUrl: './catalog-grid.css',
 })
-export class CatalogGrid {}
+export class CatalogGrid {
+  @Input({ required: true }) products: CatalogProductDto[] = [];
+  @Input() isLoading: boolean = false;
+  @Output() addToCart = new EventEmitter<CatalogProductDto>();
+  @Output() toggleFavorite = new EventEmitter<CatalogProductDto>();
+}
