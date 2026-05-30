c9d33a2252931b63f9bd50c4a56891df1b10f7b2
SushynskyiVolodymyr <sushinskyvladimir@gmail.com>
2026-05-30 18:52:04 +0300

    Implement place order

---
+++ src/backend/Vevada/Vevada.Api/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Vevada.Business.Orders.Commands;
+
+namespace Vevada.Api.Controllers;
+
+[Authorize(Roles ="Client")]
+public class OrderController : BaseApiController
+{
+    public OrderController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    private int GetUserId() => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+    [HttpPost("checkout")]
+    public async Task<IActionResult> PlaceOrder(CancellationToken cancellationToken)
+    {
+        var command = new PlaceOrderCommand(GetUserId());
+        var result = await Mediator.Send(command, cancellationToken);
+
+        return HandleResult(result);
+    }
+}
