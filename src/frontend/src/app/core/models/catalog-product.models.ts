2d3706077d7ede9420ed7c2491abcb2d80f9776a
AruitEden <annaleks1205@gmail.com>
2026-05-31 13:13:04 +0300

    created ProductVariationDto and ProductDetailsDto

--- src/frontend/src/app/core/models/catalog-product.models.ts
+++ src/frontend/src/app/core/models/catalog-product.models.ts
@@ -1,7 +1,28 @@
+import { ProductSize } from '../constants/product-size';
+
 export interface CatalogProductDto {
   id: string;
   seriesId: string;
   name: string;
   price: number;
   mainImageId: string;
+}
+
+export interface ProductVariationDto {
+  id: string;
+  mainImageId: string;
+}
+
+export interface ProductDetailsDto {
+  id: string;
+  seriesId: string;
+  seriesName: string;
+  name: string;
+  shortDescription: string | null;
+  fullDescription: string | null;
+  price: number;
+  availableSizes: ProductSize[];
+  mainImageId: string;
+  galleryImageIds: string[];
+  designVariations: ProductVariationDto[];
 }
