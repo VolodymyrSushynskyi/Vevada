0d8fef16b8b05019e751a60b4cd5f4e3c0f467d4
AruitEden <annaleks1205@gmail.com>
2026-06-02 17:28:53 +0300

    implement UI for favorites page

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './features/client/pages/home/home';
 import { ProductDetails } from './features/client/pages/product-details/product-details';
 import { Cart } from './features/client/pages/cart/cart';
+import { Favorites } from './features/client/pages/favorites/favorites';
 import { ClientAuth } from './features/auth/pages/client-auth/client-auth';
 import { AdminAuth } from './features/auth/pages/admin-auth/admin-auth';
 import { ClientLayout } from './features/client/components/client-layout/client-layout';
@@ -20,6 +21,7 @@ export const routes: Routes = [
       { path: '', component: Home },
       { path: 'product/:id', component: ProductDetails },
       { path: 'cart', component: Cart },
+      { path: 'favorites', component: Favorites },
     ],
   },
   {
